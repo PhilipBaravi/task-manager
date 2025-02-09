0564acd9f3ac542146e3c3e5b893351c0718f659
Philip <f.baravi121@gmail.com>
2025-02-09 18:56:50 +0100

    Set up routes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+import express from "express";
+import { router as tasks } from "./routes/tasks.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
---
+++ controllers/tasks.js
@@ -0,0 +1,19 @@
+export const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+export const createTask = (req, res) => {
+  res.send("create task");
+};
+
+export const getTask = (req, res) => {
+  res.send("Get single task");
+};
+
+export const updateTask = (req, res) => {
+  res.send("Update a task");
+};
+
+export const deleteTask = (req, res) => {
+  res.send("Delete a task");
+};
