9d9e2bfb72a0d50910f71b607b41bc9c57802f31
Philip <f.baravi121@gmail.com>
2025-02-10 16:40:11 +0100

    Add: Patch controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -32,7 +32,19 @@ export const getTask = async (req, res) => {
 };
 
 export const updateTask = async (req, res) => {
-  res.send("Update a task");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ id: taskID, data: req.body });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export const deleteTask = async (req, res) => {
@@ -44,6 +56,6 @@ export const deleteTask = async (req, res) => {
     }
     res.status(200).json({ task });
   } catch (error) {
-    res.status(500).json({ msg: errpr });
+    res.status(500).json({ msg: error });
   }
 };
