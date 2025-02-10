3a515946c555fccc71a2a07e076fb5d7877a2202
Philip <f.baravi121@gmail.com>
2025-02-10 15:45:43 +0100

    Get all tasks, get single task controller logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 import { Task } from "../models/Task.js";
 
-export const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export const createTask = async (req, res) => {
@@ -13,8 +18,17 @@ export const createTask = async (req, res) => {
   }
 };
 
-export const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+export const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 export const updateTask = (req, res) => {
